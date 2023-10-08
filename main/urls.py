96ad5bb1deb9c5de997f6e67c681b4ad5b8091fb
ayush <2.ayushgupta@gmail.com>
2023-10-08 13:06:15 +0530

    initial backend

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("planets", views.planets, name="planets"),
+    path("planetarium", views.planetarium, name='planetarium'),
+    path("services", views.services, name='services'),
+    path("spaceVoyage", views.spaceVoyage, name='spaceVoyage'),
+    path("vision", views.vision, name='vision'),
+    path("contact", views.contact, name='contact'),
 ]
--- main/views.py
+++ main/views.py
@@ -6,3 +6,21 @@ from django.urls import reverse
 
 def index(request):
     return render(request, 'main/index.html')
+
+def planets(request):
+    return render(request, 'main/planets.html')
+
+def planetarium(request):
+    return render(request, 'main/planetarium.html')
+
+def services(request):
+    return render(request, 'main/services.html')
+
+def spaceVoyage(request):
+    return render(request, 'main/spaceVoyage.html')
+
+def vision(request):
+    return render(request, 'main/vision.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
